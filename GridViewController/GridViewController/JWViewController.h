082ba41de277b6ae8c203d5e6682f88c60cdc0a8
Jesper Moberg <moberg.jesper@gmail.com>
2011-10-15 22:08:40 +0200

    9x9 grid fixed

--- GridViewController/GridViewController/JWViewController.h
+++ GridViewController/GridViewController/JWViewController.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-
-@interface JWViewController : UIViewController
+#import "AQGridView.h"
+@interface JWViewController : UIViewController <AQGridViewDelegate,AQGridViewDataSource>
 
 @end
