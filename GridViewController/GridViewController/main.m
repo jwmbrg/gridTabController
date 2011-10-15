b79764553675a4ceddac302715ad5ecca5b9d0d5
Jesper Moberg <moberg.jesper@gmail.com>
2011-10-15 15:33:50 +0200

    clean xcodeproject

---
+++ GridViewController/GridViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GridViewController
+//
+//  Created by Jesper Moberg on 2011-10-15.
+//  Copyright (c) 2011 JWMM Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JWAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JWAppDelegate class]));
+    }
+}
---
+++ GridViewController/GridViewControllerTests/GridViewControllerTests.m
@@ -0,0 +1,32 @@
+//
+//  GridViewControllerTests.m
+//  GridViewControllerTests
+//
+//  Created by Jesper Moberg on 2011-10-15.
+//  Copyright (c) 2011 JWMM Software. All rights reserved.
+//
+
+#import "GridViewControllerTests.h"
+
+@implementation GridViewControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GridViewControllerTests");
+}
+
+@end
