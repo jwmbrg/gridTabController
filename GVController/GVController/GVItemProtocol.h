9049b1e0d5947f92a68d641b3c4e76ca0fc9fea7
Jesper Moberg <moberg.jesper@gmail.com>
2011-11-05 08:59:47 +0100

    Added protocol, GVItemProtocol

---
+++ GVController/GVController/GVItemProtocol.h
@@ -0,0 +1,13 @@
+//
+//  GVItemProtocol.h
+//  GVController
+//
+//  Created by Jesper Moberg on 2011-11-04.
+//  Copyright (c) 2011 JWMM Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol GVItemProtocol <NSObject>
+
+@end
